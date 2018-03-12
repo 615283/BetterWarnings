e8f615650ff7e7a38d72274d36cdcd79d2bfb58d
James <615283@users.noreply.github.com>
2018-03-12 17:49:33 +0000

    Redone filesystem

--- BetterWarningsL/src/com/georlegacy/general/betterwarnings/ColorUtil.java
+++ BetterWarningsL/src/com/georlegacy/general/betterwarnings/ColorUtil.java
@@ -1,10 +1,11 @@
 package com.georlegacy.general.betterwarnings;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 public class ColorUtil {
 	
-	public static String color(String string) {
+	public static String color(String string, Player p) {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
 
